0d333ac76ba3f8acd9ef04c58aa3ac4ec10dd1cf
Aqua-Super <firdosjamal27@gmail.com>
2026-08-24 23:57:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,40 @@
-const CACHE_NAME = 'aqua-super-pwa-v1';
-const CORE = ['./', './index.html', './manifest.webmanifest'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+const CACHE_NAME = "aqua-super-v2026-08-24-1139";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./Aqua_Super.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+    fetch(event.request)
+      .then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match("./index.html")))
   );
 });
