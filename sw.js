433234c65fe6c1d2fa1dfba66d052f5ab181c970
Aqua-Super <firdosjamal27@gmail.com>
2026-08-25 21:11:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="aqua-super-final-classic-drop-2026-08-25";
+const CACHE_NAME="aqua-super-final-install-migration-174-2026-08-25";
 const APP_FILES=["./","./index.html","./Aqua_Super.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
